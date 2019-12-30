b8af83f795e51df651c13fc8a659341a9b0d00b0
David Chua <dp_chua@hotmail.com>
2019-12-29 21:16:49 -0500

    array operations

--- Master the Coding Interview Data Structures and Algorithms/Arrays.js
+++ Master the Coding Interview Data Structures and Algorithms/Arrays.js
@@ -14,3 +14,7 @@ strings.pop(); // ['a', 'b', 'c']; // O(1)
 
 // unshift - adds item in beginning of an array
 strings.unshift('x'); // ['x', 'a', 'b', 'c']; --> Time Complexity: O(n) as this adds data in beginning and moves the rest by an index of +1.
+
+//splice - adds item in middle
+// first value is where splice happens, second value is how many items to delete,  third value onwards is what is to get added.
+strings.splice(2,0, 'alien'); // ['x', 'a', 'alien', 'b', 'c'];
