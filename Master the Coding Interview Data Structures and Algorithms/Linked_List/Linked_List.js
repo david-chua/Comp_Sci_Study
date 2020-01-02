aa516c6e4fa315b604e7100063c786b182a5308b
David Chua <dp_chua@hotmail.com>
2020-01-01 21:42:50 -0500

    starting linkedList js

---
+++ Master the Coding Interview Data Structures and Algorithms/Linked_List/Linked_List.js
@@ -0,0 +1,28 @@
+// 10 --> 5 --> 16
+
+/* example structure
+let myLinkedList = {
+  head: {
+    value: 10,
+    next: {
+      value: 5,
+      next: {
+        value: 16,
+        next: null
+      }
+    }
+  }
+}
+
+*/
+
+class LinkedList {
+  constructor(value){
+    this.head = {
+      value: value,
+      next: null 
+    }
+  }
+}
+
+const myLinkedList = new LinkedList(10)
