602a886bbe6e6051fe545b9a271c12b4b038a586
David Chua <dp_chua@hotmail.com>
2020-01-08 21:16:20 -0500

    fibonacci recursion

---
+++ Master the Coding Interview Data Structures and Algorithms/Algorithms/Fibonacci.js
@@ -0,0 +1,17 @@
+// Given a number N return the index value of the Fibonacci sequence, wehre the sequence is:
+
+// 0, 1,1,2,3,5,8,13,21,34,55,89, 114...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 > 2 + 3
+
+function fibonacciIterative(n){
+
+}
+
+fibonacciIterative(3);
+
+function fibonacciRecursive(n){
+  if (n < 2){
+    return n;
+  }
+  return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+}
