1e67bd9a773a0ccf74a8298343f45ad1e5cb0252
David Chua <dp_chua@hotmail.com>
2020-01-10 19:55:04 -0500

    starting quicksort

---
+++ Master the Coding Interview Data Structures and Algorithms/Algorithms/Sorting/QuickSort.js
@@ -0,0 +1,12 @@
+// Quick Sort - O(n log n)
+// Quick Sort uses pivoting technique until they are sorted.
+
+//  3 7 8 5 2 1 9 5 4
+// place item to left or right of pivot
+//3 1 2     4     5 8 9 5 7
+// use 2 as a pivot  and use 7 as a pivot
+
+//123   4      55 7 98
+//              use 8 as pivot
+//                  89
+// 123455789                  
