fefa9642d774c3259f046d354a479b0f0b020b0e
David <dp_chua@hotmail.com>
2020-09-11 00:50:50 -0400

    Getting Started With JS

---
+++ GettingStartedWithJavaScript/Operations.js
@@ -0,0 +1,14 @@
+Binary Operators - contains two arguments
+
+3 + 4 \\ 7
+43 -1  \\ 42
+
+"David " + "Chua" \\ "David Chua"
+
+!false  \\ true
+
+3.0 == 3  \\ both numbers so equal to each other.
+
+3 < 4
+
+true || false
